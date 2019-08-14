bbb046b35ea2f47a7c3df69e5d9aa94d427304ba
Hui Shao <huiran.shao@sfgov.org>
2019-08-14 14:43:12 -0700

    tests for CSR-375

--- tests/test_permit_list.py
+++ tests/test_permit_list.py
@@ -1,4 +1,4 @@
-# pylint: disable=redefined-outer-name
+# pylint: disable=redefined-outer-name, too-many-lines
 """Tests for permit list"""
 import json
 import pytest
@@ -58,17 +58,19 @@ def get_expected_permit_list():
     """returns expected permit list from mock"""
     return json.loads("""[{"application_id": "P-2187964",
         "business_name": "Bestest Buddy Bud", "dba_name": "TESTblah",
-        "address": "420 Bud St, SF, California 94102", "parcel": "no idea", "status": "submitted", "referred": "Planning Department, Department of Public Health, Mayor's Office of Disability", "cultivator or grower (indoor)": "submitted", "distributor (cultivation to retailer)": "submitted", "retailer (medical and adult use)": "submitted", "manufacturer (nonvolatile)": "submitted"},
+        "address": "420 Bud St, SF, California 94102", "parcel": "no idea", "status": "under construction", "referred": "Planning Department, Department of Public Health, Mayor's Office of Disability", "cultivator or grower (indoor)": "under construction", "distributor (cultivation to retailer)": "under construction", "retailer (medical and adult use)": "under construction", "manufacturer (nonvolatile)": "under construction"},
         {"application_id": "TESTblah2", "business_name": "Bestest Buddy Bud Bud", "dba_name": "Bestest Buddy Bud Bud","address": "421 Bestest Bud St, San Francisco, California 94103", "parcel": "no idea", "status": "submitted", "referred": "", 
         "cultivator or grower (indoor)": "submitted", "distributor (cultivation to retailer)": "submitted", "manufacturer (nonvolatile)": "submitted", "manufacturer (volatile) (nonmicrobusiness)": "submitted", 
         "retailer (medical and adult use)": "submitted", "delivery only retail (medical and adult use)": "submitted", 
         "medical retailer (medical only)": "submitted", 
-        "testing laboratory (nonmicrobusiness)": "submitted"}]""")
+        "testing laboratory (nonmicrobusiness)": "submitted"},
+        {"application_id": "TESTblah3", "business_name": "Bestest Buddy Bud Bud Bud", "dba_name": "Bestest Buddy Trade Bud Bud Bud","address": "421 Bestest Bud St, San Francisco, California 94103", "parcel": "no idea", "status": "processing", "referred": "", 
+        "delivery only retail (medical and adult use)": "processing"}]""")
 
 def get_expected_legacy_permit_list():
     """returns legacy expected permit list from mock"""
     return json.loads("""{"TESTBLAH P-2187964": {"application_id": "P-2187964",
-    "dba_name": "TESTblah", "address": "420 Bud St, SF, California 94102", "parcel": "no idea", "activities": "retailer (medical and adult use)", "referring_dept": "Planning Department, Department of Public Health, Mayor's Office of Disability", "status": "Submitted"}, "BESTEST BUDDY BUD BUD TESTBLAH2": {"application_id": "TESTblah2", "dba_name": "Bestest Buddy Bud Bud", 
+    "dba_name": "TESTblah", "address": "420 Bud St, SF, California 94102", "parcel": "no idea", "activities": "retailer (medical and adult use)", "referring_dept": "Planning Department, Department of Public Health, Mayor's Office of Disability", "status": "Under Construction"}, "BESTEST BUDDY BUD BUD TESTBLAH2": {"application_id": "TESTblah2", "dba_name": "Bestest Buddy Bud Bud", 
     "address": "421 Bestest Bud St, San Francisco, California 94103", "parcel": "no idea", 
     "activities": "retailer (medical and adult use), delivery only retailer (medical and adult use), medicinal cannabis retailer (medical only)", 
     "referring_dept": "", "status": "Submitted"}}""")
@@ -306,7 +308,7 @@ def get_mock_sd_response():
         "num_ratings": 0,
         "created_at": "2019-01-22T23:15:21.123Z",
         "updated_at": "2019-01-22T23:48:06.693Z",
-        "status": "Submitted",
+        "status": "Build-out",
         "labels": [
             "Fire - Approved w/ Conditions",
             "Fire - Denied",
@@ -564,6 +566,248 @@ def get_mock_sd_response():
         "created_at": "2019-01-22T23:55:19.271Z",
         "updated_at": "2019-01-23T00:03:21.850Z",
         "status": "Submitted",
+        "labels": [],
+        "responder_language": "en",
+        "responder": {
+            "name": "TESThello3",
+            "email": "hello@test.test"
+        },
+        "deleted_at": null,
+        "submission_source": {
+            "type": "embedded",
+            "hostname": "test.test"
+        }
+    },
+    {
+        "id": 2188013,
+        "sequential_id": 48,
+        "project_id": 6360,
+        "form_id": 5804,
+        "initial_response_id": null,
+        "pretty_id": "o8Ce6j902ZkF",
+        "submitted_at": "2019-01-23T00:03:21.839Z",
+        "responses": {
+            "uqqrsogr": "TESTblah3",
+            "rhakmnq6": "bestesty buddy bud bud bud",
+            "9hm9uaua": "(162) 676-54614",
+            "n5133tdk": null,
+            "08l0rlqw": null,
+            "q6ou2l7j": null,
+            "tgbebquc": {
+                "checked": [
+                    "I ran a cannabis business that was previously shut down by federal authorities"
+                ]
+            },
+            "xrttaasn": null,
+            "u2wf93ww": null,
+            "ja0ciw60": null,
+            "o05kgf85": null,
+            "r0df69hg": null,
+            "jjswbsb5": null,
+            "g2arrgkt": null,
+            "se8hjyk9": null,
+            "09d8gnzx": null,
+            "hc0uyp4a": null,
+            "lo1nx04d": null,
+            "gs95ox9m": null,
+            "6ma0d8qo": null,
+            "fkpuv8fj": "1234567",
+            "t00kheyd": "Bestest Buddy Bud Bud Bud",
+            "60w4ep9y": "Bestest Buddy Trade Bud Bud Bud",
+            "kbqz4189": {
+                "city": "San Francisco",
+                "state": "California",
+                "street": "421 Bestest Bud St",
+                "country": "US",
+                "zipcode": "94103"
+            },
+            "kby1cm3l": {
+                "state": "California",
+                "country": "US"
+            },
+            "83jot6nz": null,
+            "6rk0z78i": null,
+            "qvtkw5bs": "8-4",
+            "6pxmfzgu": "2",
+            "dwbppnt7": "na",
+            "7h6geop6": "nananana batman",
+            "eyhoauj6": null,
+            "5okrxcom": null,
+            "kvrgbqrl": "no idea",
+            "icb0n2ae": null,
+            "9u16p44i": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "2sxifp1f": null,
+            "nabxbdpd": null,
+            "3rbls8p4": null,
+            "d578wxyj": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "zya7t8oq": null,
+            "xq226tmz": null,
+            "sa2zydz0": null,
+            "9vl1pyuz": null,
+            "kha0r0mz": null,
+            "g56iehgs": null,
+            "52k8jgnm": {
+                "checked": [
+                    "Own"
+                ]
+            },
+            "996qknlk": null,
+            "maxiy31h": null,
+            "i7r363tl": null,
+            "imkcygd1": null,
+            "4w4580ih": null,
+            "efckg74t": null,
+            "n0prcwm1": null,
+            "khuna9w8": null,
+            "38p27qkg": "23425",
+            "thoqrrt8": null,
+            "gzq8jno5": "Retail",
+            "vqrr0kms": "1",
+            "71tw8jio": "Retail",
+            "e9dp28ij": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "cbqd2wd2": null,
+            "e75o089r": null,
+            "umt9408s": null,
+            "3hlo4we9": {
+                "checked": [
+                    "Non-profit"
+                ]
+            },
+            "pgu6yasm": null,
+            "ncgqqac8": null,
+            "zhxaznpk": [
+                {
+                    "id": "aytNYzpHV2Heyd-Buon9oilkBzXi3CDu",
+                    "filename": "discovery-matrix.pdf"
+                }
+            ],
+            "auywweg9": null,
+            "3kir7v24": null,
+            "dlf5bh94": null,
+            "wj1tb99y": [
+                {
+                    "4e9aclly": null,
+                    "4m3npp4g": "World",
+                    "5o4d3rwh": null,
+                    "5ubp7jpr": {
+                        "city": "Lafayette",
+                        "state": "California",
+                        "street": "524 Main ST",
+                        "country": "US",
+                        "zipcode": "94549"
+                    },
+                    "ba6ndrs8": "100",
+                    "c4umqxsj": "Me",
+                    "e40uzviy": {
+                        "cents": ""
+                    },
+                    "eqen8bu4": "hello@test.test",
+                    "ftx0x558": "hello",
+                    "g5gtndt1": "Bestest buddy bud",
+                    "jg7lxezk": "(162) 676-54614",
+                    "jo6ntur1": "",
+                    "k9gpk4t7": {
+                        "day": "22",
+                        "year": "1984",
+                        "month": "01"
+                    },
+                    "nd3yl1ji": "USS",
+                    "ozmtluwe": {
+                        "checked": [
+                            "No"
+                        ]
+                    },
+                    "zxwxahiv": null
+                }
+            ],
+            "1thsvudu": true,
+            "sv99tz3b": null,
+            "e6t7r88q": null,
+            "5v6jw6uo": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "2j58le1f": null,
+            "72solxvr": null,
+            "709fnemv": null,
+            "6au0rjqk": null,
+            "oiulv1ys": null,
+            "jwzvxies": null,
+            "1ob60i64": null,
+            "t96cyy3o": null,
+            "w6729h70": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "9e2srivj": null,
+            "yse4clw9": null,
+            "m63ebprg": null,
+            "7uo5xofj": null,
+            "2pn70w6i": null,
+            "2padwkl7": null,
+            "n067esa5": null,
+            "u14mm1zj": null,
+            "6jkymg77": {
+                "checked": [
+                    "No"
+                ]
+            },
+            "mnoi8x7y": null,
+            "cnnb8od5": null,
+            "3i4snj9q": null,
+            "w1nznx8c": null,
+            "p5gsemmf": {
+                "checked": [
+                    "Yes"
+                ]
+            },
+            "dd8a5g7g": {
+                "checked": [
+                    "delivery only retail (medical and adult use)"
+                ]
+            },
+            "ujpoc5lf": null,
+            "x9fv4frg": {
+                "checked": [
+                    "Allow onsite consumption of prepackaged cannabis products",
+                    "Allow onsite consumption of cannabis products that your business will prepare"
+                ]
+            },
+            "as35515c": null,
+            "mb0z9dh6": null,
+            "p9q8hv7s": null,
+            "sbbjw5u5": {
+                "checked": [
+                    "Cultivation: Indoor (small) - Type 2A"
+                ]
+            },
+            "s0szphc9": null,
+            "bfp95f77": null,
+            "94sg2b6u": true,
+            "3kudziwl": true,
+            "al4ch3m4": null,
+            "dsfu4k6f": null
+        },
+        "rating_aggregates": {},
+        "average_rating": null,
+        "num_ratings": 0,
+        "created_at": "2019-01-22T23:55:19.271Z",
+        "updated_at": "2019-01-23T00:03:21.850Z",
+        "status": "Processing",
         "labels": [],
         "responder_language": "en",
         "responder": {
